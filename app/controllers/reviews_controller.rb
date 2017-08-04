fbb7a1cbf0ca888f3c853b2cbf6b4a5eaf3292f3
lpenstone <laura.penstone@queensu.ca>
2017-08-04 20:38:46 +0000

    Reviews save to database. Display error message

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,25 @@
+class ReviewsController < ApplicationController
+
+  def create
+    @review = Review.new(
+      rating: review_params[:rating],
+      description: review_params[:description],
+      user_id: current_user.id,
+      product_id: params["product_id"]
+      )
+    if @review.save
+      redirect_to product_path(@review.product), notice: 'Review posted!'
+    else
+      @product = @review.product
+      @reviews = @product.reviews
+      puts "comment did not save"
+      render "products/show"
+    end
+  end
+
+  private
+    def review_params
+      params.require(:review).permit(:rating, :description)
+    end
+
+end
