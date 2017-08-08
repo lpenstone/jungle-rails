ba6db6c481d0c39367570f2a5ff328fc26543706
lpenstone <laura.penstone@queensu.ca>
2017-08-08 18:44:47 +0000

    Check if users can add items to cart

--- spec/features/add_to_carts_spec.rb
+++ spec/features/add_to_carts_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
-RSpec.feature "AddToCarts", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "AddToCarts", type: :feature, js: true do
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They add to cart" do
+    # ACT
+    visit root_path
+
+    page.first("footer a").click
+    sleep 5
+
+    # VERIFY
+    page.first('#cart').text.should match('My Cart (1)')
+
+    # DEBUG
+    save_screenshot('add_to_cart.png')
+
+    end
 end
